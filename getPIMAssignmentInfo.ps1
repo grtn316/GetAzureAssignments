4ff54260a5c518b965433e6eb964fdf00264ad30
Corey Callaway <coreylcallaway@gmail.com>
2023-05-05 18:27:06 -0400

    Update getPIMAssignmentInfo.ps1

--- getPIMAssignmentInfo.ps1
+++ getPIMAssignmentInfo.ps1
@@ -8,7 +8,7 @@ param (
 #region main
 $currentDir = $(Get-Location).Path
 $currentDT = Get-Date -Format "yyyy-MM-dd"
-$oFile = "$($currentDir)\PIMAssignmentInfo_$($currentDT).csv"
+$oFile = "$($currentDir)\PIMAssignmentInfo_$($SubscriptionId)_$($currentDT).csv"
 
 $EligiblePIMData = @()
 
